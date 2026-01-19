4d87433cf843e1355fb82bdd0aa46539d77418a8
cemeterysecrets-sketch <cemeterysecrets@gmail.com>
2026-01-19 12:26:14 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import PrayerApp from "./PrayerApp";
+import PrayerApp from "./PrayerApp.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
