824fdc5b79f733bbbb6d3f64d73cb49d1a91b8a1
cemeterysecrets-sketch <cemeterysecrets@gmail.com>
2026-01-19 12:17:18 -0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import PrayerApp from "./PrayerApp";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <PrayerApp />
-  </React.StrictMode>
-);
