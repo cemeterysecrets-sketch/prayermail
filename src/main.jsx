965dd42cb52f6090a576f6c06d4ba863c6c2a1ae
cemeterysecrets-sketch <cemeterysecrets@gmail.com>
2026-01-21 14:31:59 -0500

    Remove routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router-dom";
 import PrayerApp from "./PrayerApp.jsx";
-import AnswerPrayer from "./AnswerPrayer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<PrayerApp />} />
-        <Route path="/answer" element={<AnswerPrayer />} />
-      </Routes>
-    </HashRouter>
+    <PrayerApp />
   </React.StrictMode>
 );
