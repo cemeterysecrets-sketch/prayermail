9458a272742ad1db34cd6ec44ad44159bce8327c
cemeterysecrets-sketch <cemeterysecrets@gmail.com>
2026-01-21 14:06:42 -0500

    Fix HashRouter closing tag

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import PrayerApp from "./PrayerApp.jsx";
 import AnswerPrayer from "./AnswerPrayer.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <HashRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<PrayerApp />} />
         <Route path="/answer" element={<AnswerPrayer />} />
       </Routes>
-<HashRouter>
+    </HashRouter>
   </React.StrictMode>
 );
