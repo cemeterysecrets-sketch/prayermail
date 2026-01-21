e7ef0eedc6da85009f3d96b2af36b2632f94e68a
cemeterysecrets-sketch <cemeterysecrets@gmail.com>
2026-01-21 18:39:51 -0500

    Fix main.jsx entry point

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import PrayerApp from "./PrayerApp.jsx";
 
-const root = document.getElementById("root");
-
-if (!root) {
-  throw new Error("Root element not found");
-}
-
-ReactDOM.createRoot(root).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <PrayerApp />
   </React.StrictMode>
 );
+;
