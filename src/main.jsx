aee6d484848f07763c1f6823baad890c2427e967
cemeterysecrets-sketch <cemeterysecrets@gmail.com>
2026-01-21 13:43:07 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import PrayerApp from "./PrayerApp.jsx";
 import AnswerPrayer from "./AnswerPrayer.jsx";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+   <HashRouter>
       <Routes>
         <Route path="/" element={<PrayerApp />} />
         <Route path="/answer" element={<AnswerPrayer />} />
       </Routes>
-    </BrowserRouter>
+<HashRouter>
   </React.StrictMode>
 );
